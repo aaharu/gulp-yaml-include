dac38e4bdb9d50f5620c2eb848b18f2fcb401778
Haruaki OTAKE <aaharu@hotmail.com>
2019-07-11 16:46:10 +0900

    replace gulp-util

--- tests/test.js
+++ tests/test.js
@@ -4,96 +4,92 @@ var path = require("path"),
     fs = require("fs"),
     assert = require("assert"),
     es = require("event-stream"),
-    gutil = require("gulp-util"),
-    PassThrough = require("stream").PassThrough,
+    Vinyl = require("vinyl"),
     index = require("../src/index");
 
 describe("gulp-yaml-include", function () {
     it("should work in buffer mode", function (done) {
-        var stream = index();
-        var testFile = new gutil.File({
-                base: __dirname,
-                cwd: __dirname,
-                path: path.join(__dirname, 'test.yaml'),
-                contents: fs.readFileSync(path.join(__dirname, "test.yaml"))
-            });
-
-        stream.on("data", function () {
-            assert.equal("paths:\n  a: b\n", testFile.contents.toString());
-        });
-
-        stream.on("end", function () {
-            done();
-        });
-
-        stream.write(testFile);
-        stream.end();
-    });
-
-    it("should work in stream mode", function (done) {
-        var stream = index();
-        var testStream = new PassThrough();
-        var testFile = new gutil.File({
+        var myStream = index();
+        var testFile = new Vinyl({
             base: __dirname,
             cwd: __dirname,
             path: path.join(__dirname, 'test.yaml'),
-            contents: testStream
-        });
-        testStream.write(fs.readFileSync(path.join(__dirname, "test.yaml")));
-        testStream.write(Buffer.from("\n"));
-        testStream.write(Buffer.from("c: 1"));
-        testStream.end();
-
-        stream.on("data", function (newFile) {
-            newFile.pipe(es.wait(function(err, data) {
-                assert.equal("paths:\n  a: b\nc: 1\n", data.toString());
-            }));
+            contents: fs.readFileSync(path.join(__dirname, "test.yaml"))
         });
 
-        stream.on("end", function () {
+        myStream.on("data", function (file) {
+            assert.ok(file.isBuffer());
+            assert.equal(testFile.contents.toString(), "paths:\n  a: b\n");
+        });
+
+        myStream.on("end", function () {
             done();
         });
 
-        stream.write(testFile);
-        stream.end();
+        myStream.write(testFile);
+        myStream.end();
+    });
+
+    it("should work in stream mode", function (done) {
+        var myStream = index();
+        var testFile = new Vinyl({
+            base: __dirname,
+            cwd: __dirname,
+            path: path.join(__dirname, 'test.yaml'),
+            contents: fs.createReadStream(path.join(__dirname, 'test.yaml'))
+        });
+
+        myStream.on("data", function (newFile) {
+            assert.ok(newFile.isStream());
+            newFile.contents.pipe(es.wait(function(err, data) {
+                assert.equal(data.toString(), "paths:\n  a: b\n");
+            }));
+        });
+
+        myStream.on("end", function () {
+            done();
+        });
+
+        myStream.write(testFile);
+        myStream.end();
     });
 
     it("should let null files pass through", function (done) {
-        var stream = index(),
+        var myStream = index(),
             n = 0;
-        stream.pipe(es.through(function (file) {
+        myStream.pipe(es.through(function (file) {
             assert.equal(file.path, "null.yaml");
             assert.equal(file.contents,  null);
             n++;
         }, function () {
-            assert.equal(n, 1);
+            assert.equal(1, n);
             done();
         }));
-        stream.write(new gutil.File({
+        myStream.write(new Vinyl({
             path: "null.yaml",
             contents: null
          }));
-        stream.end();
+         myStream.end();
     });
 
     it("should work subdir inc", function (done) {
-        var stream = index();
-        var testFile = new gutil.File({
-                base: __dirname,
-                cwd: __dirname,
-                path: path.join(__dirname, 'base.yaml'),
-                contents: fs.readFileSync(path.join(__dirname, "base.yaml"))
-            });
-
-        stream.on("data", function () {
-            assert.equal("version: 1\nsub:\n  hoge: fuga\n", testFile.contents.toString());
+        var myStream = index();
+        var testFile = new Vinyl({
+            base: __dirname,
+            cwd: __dirname,
+            path: path.join(__dirname, 'base.yaml'),
+            contents: fs.readFileSync(path.join(__dirname, "base.yaml"))
         });
 
-        stream.on("end", function () {
+        myStream.on("data", function () {
+            assert.equal(testFile.contents.toString(), "version: 1\nsub:\n  hoge: fuga\n");
+        });
+
+        myStream.on("end", function () {
             done();
         });
 
-        stream.write(testFile);
-        stream.end();
+        myStream.write(testFile);
+        myStream.end();
     });
 });
